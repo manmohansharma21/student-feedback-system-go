5d3c685ad997aeb94c3c289a76d3c9aeae128787
Manmohan_Sharma <manmohansharma4854@gmail.com>
2025-05-25 23:56:37 +0530

    updated names

--- internal/db/schema.sql
+++ internal/db/schema.sql
@@ -2,10 +2,23 @@ CREATE DATABASE feedback_db;
 
 \c feedback_db
 
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(100)
+);
+
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    course_name VARCHAR(100),
+    faculty VARCHAR(100)
+);
+
 CREATE TABLE feedback (
     id SERIAL PRIMARY KEY,
-    student_id INT,
-    course_id INT,
+    student_id INT REFERENCES students(id),
+    course_id INT REFERENCES courses(id),
     rating INT CHECK (rating >= 1 AND rating <= 5),
     comments TEXT,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
